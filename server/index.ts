bb3f200af67106e83dde9313ef006c367abe8588
Ravi Teja Pedapudi <prt15@iitbbs.ac.in>
2024-10-12 15:22:37 +0530

    server: add cors and bodyparser

--- server/index.ts
+++ server/index.ts
@@ -1,14 +1,20 @@
 import express, { type Request, type Response } from "express";
 import cors from "cors";
+import bodyParser from "body-parser";
 
 import authRouter from "./routes/auth";
-import bodyParser from "body-parser";
+
+import { logger } from "./middleware/logger";
+
+import { CLIENT_URL } from "./constants";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: CLIENT_URL, credentials: true }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.use(logger);
 
 const PORT = 3000;
 
