aee824b470d565d251028ad02d049462020c4a47
Ravi Teja Pedapudi <prt15@iitbbs.ac.in>
2024-10-16 18:12:23 +0530

    client(chore): chore files for now

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,4 +1,24 @@
 // global
 import MaxWidthWrapper from "./global/max-width-wrapper";
+import Loader from "./global/Loader";
 
-export { MaxWidthWrapper };
+// main
+import Sidebar from "./main/Sidebar";
+import Feed from "./main/Feed";
+
+// ui
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+import { Label } from "./ui/label";
+import { Toaster } from "sonner";
+
+export {
+  MaxWidthWrapper,
+  Loader,
+  Sidebar,
+  Feed,
+  Button,
+  Input,
+  Label,
+  Toaster,
+};
