6d0a014eed8599f47b828582b6f08938cad74a20
Ravi Teja Pedapudi <prt15@iitbbs.ac.in>
2024-10-16 18:14:38 +0530

    client: user type and chore

--- client/src/routes/(root)/index.lazy.tsx
+++ client/src/routes/(root)/index.lazy.tsx
@@ -1,5 +1,12 @@
-import { createLazyFileRoute } from '@tanstack/react-router'
+import { Feed, Sidebar } from "@/components";
 
-export const Route = createLazyFileRoute('/(root)/')({
-  component: () => <div>Hello /!</div>,
-})
+import { createLazyFileRoute } from "@tanstack/react-router";
+
+export const Route = createLazyFileRoute("/(root)/")({
+  component: () => (
+    <div className="grid grid-cols-[1fr_2fr_1fr] p-6">
+      <Sidebar />
+      <Feed />
+    </div>
+  ),
+});
---
+++ client/src/types/user.ts
@@ -0,0 +1,9 @@
+type User = {
+  id: number;
+  username: string;
+  email: string;
+  password: string;
+  created_at: string;
+};
+
+export default User;
