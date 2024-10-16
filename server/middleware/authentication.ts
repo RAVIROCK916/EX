67e6b98a5a5da7036d10a6198c70044e9db60ba3
Ravi Teja Pedapudi <prt15@iitbbs.ac.in>
2024-10-16 18:17:23 +0530

    server: middleware for authentication

---
+++ server/middleware/authentication.ts
@@ -0,0 +1,29 @@
+import jwt from "jsonwebtoken";
+import { NextFunction, Request, Response } from "express";
+
+export default async function isAuthenticated(
+	req: Request,
+	res: Response,
+	next: NextFunction
+) {
+	const accessToken = req.headers.authorization?.split(" ")[1];
+
+	if (!accessToken) {
+		return res.status(401).send({ message: "Unauthorized" });
+	}
+
+	jwt.verify(
+		accessToken,
+		process.env.ACCESS_TOKEN_SECRET!,
+		(err, decoded: any) => {
+			if (err) {
+				console.log(err.message);
+				return res.status(403).send({ message: "Invalid token" });
+			}
+
+			req.user = decoded.userId;
+
+			next();
+		}
+	);
+}
