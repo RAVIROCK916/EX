db365bca92d4209926d7674a26e108fd0b8b0772
Ravi Teja Pedapudi <prt15@iitbbs.ac.in>
2024-10-16 18:13:13 +0530

    client: add react redux store and auth slice

---
+++ client/src/state/reducers/auth.ts
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState: {
+    token: null,
+  },
+  reducers: {
+    setToken: (state, action) => {
+      const token = action.payload;
+      state.token = token;
+    },
+    logout: (state) => {
+      state.token = null;
+    },
+  },
+});
+
+export const { setToken, logout } = authSlice.actions;
+
+export default authSlice.reducer;
