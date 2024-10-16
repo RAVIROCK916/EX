aac85ad31c794bec5cc8de0a7d87b96763f086ab
Ravi Teja Pedapudi <prt15@iitbbs.ac.in>
2024-10-16 18:08:54 +0530

    client: config changed from main to App file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,37 @@
+import { Provider } from "react-redux";
+import { routeTree } from "./routeTree.gen";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+
+import store from "./state/store";
+
+export const router = createRouter({ routeTree });
+
+// Register the router instance for type safety
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+// TanStack Router Devtools
+// const TanStackRouterDevtools =
+// 	process.env.NODE_ENV === "production"
+// 		? () => null // Render nothing in production
+// 		: React.lazy(() =>
+// 				// Lazy load in development
+// 				import("@tanstack/router-devtools").then((res) => ({
+// 					default: res.TanStackRouterDevtools,
+// 					// For Embedded Mode
+// 					// default: res.TanStackRouterDevtoolsPanel
+// 				}))
+// 			);
+
 const App = () => {
-	return <div>App</div>;
+  return (
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  );
+  /* <TanStackRouterDevtools router={router} /> */
 };
 export default App;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,37 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { RouterProvider, createRouter } from "@tanstack/react-router";
 
-// Import the generated route tree
-import { routeTree } from "./routeTree.gen";
+import App from "./App";
 
-// Create a new router instance
-const router = createRouter({ routeTree });
+const root = document.getElementById("root");
 
-// TanStack Router Devtools
-const TanStackRouterDevtools =
-	process.env.NODE_ENV === "production"
-		? () => null // Render nothing in production
-		: React.lazy(() =>
-				// Lazy load in development
-				import("@tanstack/router-devtools").then((res) => ({
-					default: res.TanStackRouterDevtools,
-					// For Embedded Mode
-					// default: res.TanStackRouterDevtoolsPanel
-				}))
-			);
-
-// Register the router instance for type safety
-declare module "@tanstack/react-router" {
-	interface Register {
-		router: typeof router;
-	}
-}
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-	<React.StrictMode>
-		<RouterProvider router={router} />
-		{/* <TanStackRouterDevtools router={router} /> */}
-	</React.StrictMode>
+ReactDOM.createRoot(root!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 );
