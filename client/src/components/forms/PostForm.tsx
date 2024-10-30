9e0c3fa97ec19fee2b09c1a3a7d76f52b35204a4
Ravi Teja Pedapudi <prt15@iitbbs.ac.in>
2024-10-30 22:35:46 +0530

    client: add post form and feed component

---
+++ client/src/components/forms/PostForm.tsx
@@ -0,0 +1,98 @@
+import { useState } from "react";
+
+import { z } from "zod";
+
+import FileUploader from "../global/FileUploader";
+
+import { Button, Input, Label, Textarea } from "@/components";
+import { toast } from "sonner";
+
+import { SERVER_URL } from "@/constants";
+import protectedAPI from "@/lib/axios/auth";
+import { useNavigate } from "@tanstack/react-router";
+
+const PostForm = () => {
+  const [caption, setCaption] = useState<string>("");
+  const [image, setImage] = useState<string | null>(null);
+  const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
+
+  const navigate = useNavigate();
+
+  const formData = { caption, image };
+
+  const formSchema = z.object({
+    caption: z.string().min(1, "Caption is required"),
+    image: z.string().url().nullable().optional(),
+  });
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log({ formData });
+
+    // form validation
+    const formValidation = formSchema.safeParse(formData);
+
+    if (!formValidation.success) {
+      formValidation.error.errors.forEach((error) => {
+        console.log(error.message);
+
+        toast.error(error.message);
+      });
+      return;
+    }
+
+    setIsSubmitting(true);
+
+    try {
+      await protectedAPI.post(`${SERVER_URL}/posts`, formData);
+    } catch (err) {
+      console.log(err);
+      return;
+    } finally {
+      setIsSubmitting(false);
+    }
+
+    toast.success("Post added!");
+    navigate({ to: "/", replace: true });
+  };
+
+  return (
+    <form className="space-y-6">
+      <div className="space-y-2">
+        <Label htmlFor="caption">Caption</Label>
+        <Textarea
+          id="caption"
+          className="h-24 bg-neutral-900"
+          value={caption}
+          onChange={(e) => setCaption(e.target.value)}
+          placeholder="Write a caption for your post"
+        />
+      </div>
+      <div className="space-y-2">
+        <Label htmlFor="file">Add Photos</Label>
+        <FileUploader image={image} setImage={setImage} />
+      </div>
+      <div className="space-y-2">
+        <Label htmlFor="tags">Add Tags</Label>
+        <Input
+          id="tags"
+          className="bg-neutral-900 duration-0 focus-visible:border-transparent focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2"
+          placeholder="Travel, Nature, Beach, Mountains..."
+        />
+      </div>
+      <div className="flex justify-end gap-x-6">
+        <Button className="" variant="ghost">
+          Cancel
+        </Button>
+        <Button
+          className={`w-20 ${isSubmitting && "cursor-not-allowed"}`}
+          onClick={handleSubmit}
+          disabled={isSubmitting}
+        >
+          Post
+        </Button>
+      </div>
+    </form>
+  );
+};
+export default PostForm;
--- client/src/components/main/Feed.tsx
+++ client/src/components/main/Feed.tsx
@@ -1,5 +1,48 @@
+import { useEffect, useState } from "react";
+
+import Post from "@/types/post";
+
+import Loader from "../global/Loader";
+import { SERVER_URL } from "@/constants";
+import protectedAPI from "@/lib/axios/auth";
+import PostCard from "./PostCard";
+
 const Feed = () => {
-  return <div className=""></div>;
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  const fetchPosts = async () => {
+    try {
+      const response = await protectedAPI.get(`${SERVER_URL}/posts/latest`);
+      const data = await response.data;
+
+      setPosts(data);
+
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  return (
+    <div className="space-y-4">
+      <h2 className="text-3xl font-bold">Posts</h2>
+      {isLoading ? (
+        <div className="flex items-center justify-center p-6">
+          <Loader />
+        </div>
+      ) : (
+        <ul className="space-y-4">
+          {posts.length > 0 &&
+            posts.map((post) => <PostCard key={post.id} post={post} />)}
+        </ul>
+      )}
+    </div>
+  );
 };
 
 export default Feed;
